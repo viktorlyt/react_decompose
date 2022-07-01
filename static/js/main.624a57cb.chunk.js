daf7557a26e3e0b5221d8c7b721ecfe79e3689c5
viktorlyt <viktorlyt@gmail.com>
2022-07-01 12:46:04 +0300

    Updates

--- static/js/main.624a57cb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreact_decompose=this.webpackJsonpreact_decompose||[]).push([[0],[,,,,,,,,function(n,t,c){},function(n,t,c){},,function(n,t,c){},function(n,t,c){},function(n,t,c){"use strict";c.r(t);c(1);var e=c(3),o=c.n(e),s=(c(8),c(9),c(0));var r=function(){};c(11);var a=function(){};c(12);var i=function(){};var u=function(){return Object(s.jsxs)("main",{className:"app",children:[Object(s.jsx)(i,{}),Object(s.jsx)(a,{}),Object(s.jsx)(r,{})]})};o.a.render(Object(s.jsx)(u,{}),document.getElementById("root"))}],[[13,1,2]]]);
-//# sourceMappingURL=main.624a57cb.chunk.js.map
